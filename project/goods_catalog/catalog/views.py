a360aabb22b82eabedee0f1fd515bc61fe098e0c
Krollbotid <krollbotid@gmail.com>
2024-10-31 13:46:13 +0300

    Added: searcher, filter

--- project/goods_catalog/catalog/views.py
+++ project/goods_catalog/catalog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.db.models import Q
+from .models import Product, Producer, User  # предполагается, что эти модели уже подключены
 from django.http import HttpResponse
 
 # TMP
@@ -40,31 +42,57 @@ cards = [
     },
 ]
 
-# TMP
-item = {
-    'id': 324,
-    'seller': 'Ilya',
-    'producer': 'Basynya',
-    'full_name': 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.',
-    'short_name': 'Lorem ipsum dolor sit amet.',
-    'description': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque amet magni quos expedita nemo sequi voluptas facere nihil ullam tempora voluptatem, corrupti exercitationem qui molestias doloremque ipsam minus dolor quis.',
-    'created_at': '2024-10-26',
-    'quantity': 3,
-    'price': 299.99,
-}
-
 def index(request):
     # return HttpResponse("<h1>Index</h1>")
     return render(request, 'index.html')
 
 def catalog(request):
+    query = request.GET.get('query', '')
+    producer_id = request.GET.get('producer')
+    seller_id = request.GET.get('seller')
+    min_price = request.GET.get('min_price')
+    max_price = request.GET.get('max_price')
+    min_quantity = request.GET.get('min_quantity')
+
+    products = Product.objects.all()
+
+    # Поиск
+    if query:
+        products = products.filter(
+            Q(full_name__icontains=query) | 
+            Q(short_name__icontains=query) | 
+            Q(description__icontains=query) |
+            Q(producer_id__full_name__icontains=query) | 
+            Q(seller_id__login__icontains=query)
+        )
+
+    # Фильтрация
+    if producer_id:
+        products = products.filter(producer_id=producer_id)
+    if seller_id:
+        products = products.filter(seller_id=seller_id)
+    if min_price:
+        products = products.filter(price__gte=min_price)
+    if max_price:
+        products = products.filter(price__lte=max_price)
+    if min_quantity:
+        products = products.filter(quantity__gte=min_quantity)
+
+    # Передаем списки производителей и продавцов в шаблон для фильтрации
+    producers = Producer.objects.all()
+    sellers = User.objects.all()
+
     return render(request, 'catalog/catalog.html', context={
         'page_title': 'Catalog',
-        'cards': cards, # брать из БД
+        'cards': products,
+        'producers': producers,
+        'sellers': sellers,
     })
 
+
 def product(request, id):
+    product = get_object_or_404(Product, id=id)
     return render(request, 'catalog/product.html', context={
         'page_title': 'Product',
-        'product': item, # брать из БД, найдя по id и склеив с именами производителя и продавца (по fk)
+        'product': product,
     })
