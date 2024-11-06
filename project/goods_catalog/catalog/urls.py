30360b9f61bd65e30a4ef0b456056512ea9d3049
Krollbotid <krollbotid@gmail.com>
2024-11-06 09:36:13 +0300

    Added: tests for catalog, users, support apps

--- project/goods_catalog/catalog/urls.py
+++ project/goods_catalog/catalog/urls.py
@@ -4,8 +4,8 @@ from catalog import views
 urlpatterns = [
     path('', views.HomeView.as_view(), name = 'index'),
     path('catalog/', views.catalog, name = 'catalog'),
-    path('product/<int:id>', views.ProductView.as_view(), name = 'product'),
+    path('product/<int:id>/', views.ProductView.as_view(), name = 'product'),
     path('add/', views.AddProduct.as_view(), name = 'add'),
-    path('update/<int:id>', views.UpdateProduct.as_view(), name = 'update'),
-    path('delete/<int:id>', views.DeleteProduct.as_view(), name = 'delete'),
+    path('update/<int:id>/', views.UpdateProduct.as_view(), name = 'update'),
+    path('delete/<int:id>/', views.DeleteProduct.as_view(), name = 'delete'),
 ]
--- project/goods_catalog/users/tests.py
+++ project/goods_catalog/users/tests.py
@@ -1,3 +1,44 @@
 from django.test import TestCase
+from django.urls import reverse
+from .forms import RegisterForm, LoginForm
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class UserModelTest(TestCase):
+    def test_user_creation(self):
+        user = User.objects.create_user(username='testuser', password='IhateDjango228', email='user@test.com')
+        self.assertEqual(user.username, 'testuser')
+
+class RegisterFormTest(TestCase):
+    def test_valid_register_form(self):
+        form = RegisterForm(data={
+            'username': 'testuser', 'email': 'user@test.com', 'phone': '1234567890',
+            'password1': 'IhateDjango228', 'password2': 'IhateDjango228'
+        })
+        self.assertTrue(form.is_valid())
+
+class LoginFormTest(TestCase):
+    def setUp(self):
+        User.objects.create_user(username='testuser', password='IhateDjango228', email='user@test.com')
+        
+    def test_valid_login_form(self):
+        form = LoginForm(data={'username': 'testuser', 'password': 'IhateDjango228'})
+        self.assertTrue(form.is_valid())
+
+class UserViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='user', password='PasswordValidationisShit228')
+
+    def test_login_view(self):
+        response = self.client.get(reverse('users:login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_view(self):
+        response = self.client.get(reverse('users:register'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_view(self):
+        self.client.login(username='user', password='PasswordValidationisShit228')
+        response = self.client.get(reverse('users:logout'))
+        self.assertRedirects(response, reverse('users:login'))
