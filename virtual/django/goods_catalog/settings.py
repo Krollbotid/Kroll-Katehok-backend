2f5ac002f357f65bd120974d7a77fd0901df2799
Krollbotid <krollbotid@gmail.com>
2024-12-19 11:14:49 +0300

    Removed: mail settings from settings.py. Added: flake8 to django poetry settings for further testing

--- virtual/django/goods_catalog/settings.py
+++ virtual/django/goods_catalog/settings.py
@@ -140,14 +140,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'markuskonev@gmail.com'
-EMAIL_HOST_PASSWORD = 'hexj deti oynp dsqh'
-DEFAULT_FROM_EMAIL = 'noreplyGrusha@gmail.com'
-
 LOGIN_REDIRECT_URL = '/' # TMP
 LOGOUT_REDIRECT_URL = 'users:logout'
 LOGIN_URL = 'users:login'
